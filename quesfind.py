eefa09086764b0b5f81133d67263ba8c82db95fd
Nova ⚡ Manish <67814686+DynamicManish@users.noreply.github.com>
2021-04-09 12:51:21 +0530

    Update quesfind.py

--- quesfind.py
+++ quesfind.py
@@ -1,55 +1,24 @@
-import random as r 
-import time as t 
-random_value = r.randint(2,9)
+#Hi, This is a question finder
+#It will take inputs, will check if it is an interrogative user_input, will make it capitalized cased and will arrange it in the form of a sentence
 
-guesses = 0 #user haven't made any guess yet!
-value = 0 
-
-user_name = input('Howdy?May I know your name? ').strip().capitalize()
-t.sleep(1)
-print("Hello,"+"{}!".format(user_name))
-t.sleep(1)
-ques_choice = input('Are you ready to guess?[Y/N]:').strip()
-
-if ques_choice == 'y':
-    print('Hello,'+"{}".format(user_name))
-    t.sleep(1)
-    print("I am thinking of a number between 1 to 10!")
-    t.sleep(1)
-elif ques_choice == 'n':
-    print("We're sorry, Will meet you later!")
-    t.sleep(1)
-    exit()
-else:
-    print("Wrong input!")
-    t.sleep(1)
-    exit()
-
-while not (random_value == value):
-    number = int(input('Enter your guess here: '))
-    guesses = guesses+1
-    if number == random_value:
-        t.sleep(0.75)
-        print("Brilliant!")
-        print("You guessed it correctly,the correct number is {}".format(random_value))
-        if guesses == 1:
-            print("It took you {}".format(guesses)+" try in total!")
-            t.sleep(1)
-        else:
-            print("It took you {}".format(guesses)+" tries in total!")
-            t.sleep(1)
-
-        exit()
-    elif number < random_value:
-        t.sleep(1)
-        print("Guess higher")
-    elif number > random_value:
-        t.sleep(1)
-        print("Guess lower")   
+def question_finder(sentence):
+    sentence = sentence.capitalize()
+    if sentence.startswith(('What','When','How','Is','Whom','Why','Whose','Howdy')):
+        return "{}?".format(sentence)
     else:
-        t.sleep(1)
-        print("Wrong inputs!")
-        exit()      
+        return "{}.".format(sentence)
 
+user_data = []
 
+while True:
+    user_input = input("Type a sentence: ").strip()
+    Stop = user_input.lower()
+    if Stop == 'stop':
+        break
+    else:
+        user_data.append(question_finder(user_input))
 
+        
+# print(" ".join((user_data))),this line of command can be used also
+for inputs in user_data: #at this time, I am using this one :)
+    print(inputs,end=" ")
